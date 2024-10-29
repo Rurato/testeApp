46dae555a3eb0a46712a272a32278a409325bcb7
João Pedro <moreirajp0707@gmail.com>
2024-10-29 20:17:52 -0300

    foi porra

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ttt</Text>
+      <Text>slalalslas</Text>
       <StatusBar style="auto" />
     </View>
   );
